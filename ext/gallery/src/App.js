fdc5f86d789cd6f5378900304eb0fe218b7868c0
lukasz.czaplinski <czapl.luk+git@gmail.com>
2019-12-05 18:19:22 +0100

    Refactor: re-indent

--- ext/gallery/src/App.js
+++ ext/gallery/src/App.js
@@ -4,15 +4,15 @@ import Carousel, { Modal, ModalGateway } from "react-images";
 import InfiniteScroll from 'react-infinite-scroller';
 
 async function photos(page) {
-    const server = process.env.NODE_ENV === 'production' ? '' : "http://localhost:9393"
-    const response = await fetch(server + '/all?page=' + page);
-    const all = await response.json();
-    return all;
+  const server = process.env.NODE_ENV === 'production' ? '' : "http://localhost:9393"
+  const response = await fetch(server + '/all?page=' + page);
+  const all = await response.json();
+  return all;
 }
 
 async function deletePhoto(img) {
-    const response = await fetch(img.src, { method: 'DELETE'});
-    return response;
+  const response = await fetch(img.src, { method: 'DELETE'});
+  return response;
 }
 
 function App() {
@@ -40,45 +40,47 @@ function App() {
     setViewerIsOpen(true);
   }, []);
 
-    const closeLightbox = useCallback(() => {
-        const currentPhoto = currentPhotos[currentImage];
-        const toBeDeleted = window.confirm(`Delete photo ${currentPhoto.title}?`)
-        if (toBeDeleted) {
-            deletePhoto(currentPhotos[currentImage]).then(() => {
-                // Need to create a new array, since Gallery is memoized. Would be an issue for bigger galleries.
-                const photosWithoutCurrent = currentPhotos.slice(0, currentImage).concat(...currentPhotos.slice(currentImage+1, currentPhotos.length));
-                setCurrentPhotos(photosWithoutCurrent);
-            }).catch(console.error.bind(console));
-        }
-        setCurrentImage(0);
-        setViewerIsOpen(false);
-    }, [currentImage, currentPhotos]);
+  const closeLightbox = useCallback(() => {
+    const currentPhoto = currentPhotos[currentImage];
+    const toBeDeleted = window.confirm(`Delete photo ${currentPhoto.title}?`)
+    if (toBeDeleted) {
+      deletePhoto(currentPhotos[currentImage]).then(() => {
+        // Need to create a new array, since Gallery is memoized. Would be an issue for bigger galleries.
+        const photosWithoutCurrent = currentPhotos.slice(0, currentImage).concat(...currentPhotos.slice(currentImage+1, currentPhotos.length));
+        setCurrentPhotos(photosWithoutCurrent);
+      }).catch(console.error.bind(console));
+    }
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  }, [currentImage, currentPhotos]);
+
+  const viewer = viewerIsOpen ? (
+    <Modal onClose={closeLightbox}>
+      <Carousel
+        components={{ Footer: null, NavigationPrev: null, Navigation: null}}
+        currentIndex={currentImage}
+        views={currentPhotos.map((x, idx) => ({
+          ...x,
+          src: idx === currentImage ? x.original : x.src,
+          srcset: x.srcSet,
+          caption: x.title
+        }))}
+        />
+    </Modal>
+  ) : null
 
   return (
     <div>
-        <InfiniteScroll
-            pageStart={0}
-            loadMore={loadPhotos}
-            hasMore={currentPage !== null}
-            loader={<div className="loader" key={0}>Loading ...</div>}
-            >
-                <Gallery photos={currentPhotos} onClick={openLightbox} />
-        </InfiniteScroll>
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={loadPhotos}
+        hasMore={currentPage !== null}
+        loader={<div className="loader" key={0}>Loading ...</div>}
+        >
+        <Gallery photos={currentPhotos} onClick={openLightbox} />
+      </InfiniteScroll>
       <ModalGateway>
-        {viewerIsOpen ? (
-          <Modal onClose={closeLightbox}>
-            <Carousel
-              components={{ Footer: null, NavigationPrev: null, Navigation: null}}
-              currentIndex={currentImage}
-              views={currentPhotos.map((x, idx) => ({
-                ...x,
-                src: idx === currentImage ? x.original : x.src,
-                srcset: x.srcSet,
-                caption: x.title
-              }))}
-            />
-          </Modal>
-        ) : null}
+        {viewer}
       </ModalGateway>
     </div>
   );
