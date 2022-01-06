b88b7f74e6107fe92c726935c4099cf58a69748e
Lukasz Czaplinski <git@scoiatael.dev>
2022-01-06 13:00:23 +0100

    Fix minify db location

--- scripts/minify.rb
+++ scripts/minify.rb
@@ -3,6 +3,7 @@
 require_relative '../lib/parrhasius'
 
 storage = Parrhasius::Storage.new(ARGV.first)
+index = File.expand_path(ENV.fetch('SERVE', './db') + "/index.pstore")
 
-Parrhasius::Dedup.new(db: 'db/index.pstore', dir: storage.dir, progress_bar: ProgressBar).run
+Parrhasius::Dedup.new(db: index, dir: storage.dir, progress_bar: ProgressBar).run
 Parrhasius::Minify.new(ProgressBar).run(src: storage.dir, dest: storage.dir.sub('original', 'thumbnail'))
