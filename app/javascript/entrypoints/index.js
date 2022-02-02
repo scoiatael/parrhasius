b2dee26b2467f9efff268233c20a215b1ffeb79e
Lukasz Czaplinski <git@scoiatael.dev>
2022-02-02 23:14:45 +0100

    Prepare for multiple entrypoints in js

--- app/javascript/entrypoints/index.js
+++ app/javascript/entrypoints/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./app.css";
 import '@materializecss/materialize/dist/css/materialize.min.css'
 import '@materializecss/materialize/dist/js/materialize.min.js'
-import App from "./App";
+import App from "../App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
