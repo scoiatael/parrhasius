eaac0f90811f92d70be51dba15685e32b6333218
Lukasz Czaplinski <git@scoiatael.dev>
2022-02-04 21:01:49 +0100

    fixup! Escape special characters in dir names

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -86,8 +86,8 @@ class QueriesController < ApplicationController # rubocop:todo Style/Documentati
 
   def image_url(image)
     path = Pathname.new(image.path)
-    rel = Rack::Utils.escape(path.relative? ? path.to_s : path.relative_path_from(Parrhasius::DIR).to_s)
-    request.base_url + "/image/#{rel}"
+    rel = ERB::Util.url_encode(path.relative? ? path.to_s : path.relative_path_from(Parrhasius::DIR).to_s)
+    request.base_url + "/image/#{rel}" # FIXME: upper case is broken?
   end
 
   def status(job)
